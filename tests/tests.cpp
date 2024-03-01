d6ecea56542b531bfeb9b736812f94f9ff02b88d
root <zolotykh907@gmail.com>
2024-03-01 08:02:34 +0000

    delete tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,11 +8,3 @@ TEST(SUM, int_double_TEST) {
     EXPECT_EQ(sum(0.6, 0.6), 1.2);
     EXPECT_EQ(sum(1, 1), 2);
 }
-TEST(MINUS, int_double_TEST) {
-    EXPECT_EQ(minus(1.6, 0.4), 1.2);
-    EXPECT_EQ(minus(3, 1), 2);
-}
-TEST(MUL, int_double_TEST) {
-    EXPECT_EQ(minus(1.2, 1.0), 1.2);
-    EXPECT_EQ(minus(2, 1), 2);
-}
